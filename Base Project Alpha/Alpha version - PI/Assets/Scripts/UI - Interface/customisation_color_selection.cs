6c7ccc45e6ea08acff159ddc5016c878b0bc8b60
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-03-10 20:21:30 +0100

    Alpha Version - Finalisation du choix de couleur depuis un curseur se placant sur la zone de couleur et jamais en dehors et applique la couleur en accord avec le choix sur le buggy, fonctionne bien :ok_hand:

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/customisation_color_selection.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/customisation_color_selection.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class customisation_color_selection : MonoBehaviour {
+public class customisation_color_selection : MonoBehaviour, IPointerDownHandler
+{
 
     public Slider colorPicked;
     public Material gradient3D;
+
+    public Material vehicleMainColor;
 
     private Color vehicle_color;
 
     // Use this for initialization
     void Start () {
-		
+
 	}
 	
-    public void OnPointerClick () {
-        Debug.Log(this.name);
+    public void OnPointerDown (PointerEventData pointerData) {
+        if(pointerData.position.x>=512 && pointerData.position.x <= 712 && pointerData.position.y >= 484 && pointerData.position.y <= 684)
+        {
+            GameObject.Find("cursor_texture").transform.localPosition = new Vector3(pointerData.position.x - 512, pointerData.position.y - 484, 0);
+            vehicleMainColor.color = calculate_color_general(pointerData.position.x-512, pointerData.position.y-484);
+        }
     }
 
     Color calculate_color_range(float value)
@@ -33,6 +41,17 @@ public class customisation_color_selection : MonoBehaviour {
             return new Color((value % 0.1666f) * 6, 0, 1, 1);
         else
             return new Color(1, 0, 1-((value % 0.1667f) * 6), 1);
+    }
+
+    Color calculate_color_general(float x, float y)
+    {
+
+        float vehicle_color2_x = (x / 200) * (vehicle_color.r + (1 - (y/200)) * (1 - vehicle_color.r));
+        float vehicle_color2_y = (x / 200) * (vehicle_color.g + (1 - (y / 200)) * (1 - vehicle_color.g));
+        float vehicle_color2_z = (x / 200) * (vehicle_color.b + (1 - (y / 200)) * (1 - vehicle_color.b));
+
+        Color vehicle_color2 = new Color(vehicle_color2_x, vehicle_color2_y, vehicle_color2_z);
+        return vehicle_color2;
     }
 
     // Update is called once per frame
