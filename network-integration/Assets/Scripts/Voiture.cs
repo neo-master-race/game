ef6318c7a724925edb3af1f7d5bc60cd0b9e4126
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-14 22:22:34 +0100

    added limiter and better car controls

--- network-integration/Assets/Scripts/Voiture.cs
+++ network-integration/Assets/Scripts/Voiture.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Voiture : MonoBehaviour {
 
   public bool isLocalPlayer;
+  private int limiter = 0;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,23 +15,26 @@ public class Voiture : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    if (isLocalPlayer) {
-      float verticalAxis = Input.GetAxis("Vertical");
-  		float horizontalAxis = Input.GetAxis("Horizontal");
+    limiter = limiter++ % 12;
+    if (limiter == 0) {
+      if (isLocalPlayer) {
+        float verticalAxis = Input.GetAxis("Vertical");
+    		float horizontalAxis = Input.GetAxis("Horizontal") * 5 * System.Math.Abs(verticalAxis);
 
-      Vector3 vec = transform.localPosition;
-      Vector3 vecRot = transform.localEulerAngles;
+        Vector3 vec = transform.localPosition;
+        Vector3 vecRot = transform.localEulerAngles;
 
-      vecRot.y += horizontalAxis;
-      vec += verticalAxis * transform.forward;
+        vecRot.y += horizontalAxis;
+        vec += verticalAxis * transform.forward;
 
-      transform.localPosition = vec;
-      transform.localEulerAngles = vecRot;
+        transform.localPosition = vec;
+        transform.localEulerAngles = vecRot;
 
-      if (verticalAxis != 0 || horizontalAxis != 0) {
-        Debug.Log(transform.position);
-        Debug.Log(transform.rotation);
-        Debug.Log(transform.localScale);
+        if (verticalAxis != 0 || horizontalAxis != 0) {
+          Debug.Log(transform.position);
+          Debug.Log(transform.rotation);
+          Debug.Log(transform.localScale);
+        }
       }
     }
 	}
