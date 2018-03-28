950a35a76b2a4a29bd5d88652ce7cefe44368ef8
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-03-29 00:25:51 +0200

    more accurate on initialization on waypoints

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/InTrack/Player_Info_Ingame.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/InTrack/Player_Info_Ingame.cs
@@ -31,6 +31,7 @@ public class Player_Info_Ingame : MonoBehaviour {
 
     [Header("Other Players")]
     public GameObject[] players;
+    public GameObject[] playersLeaderboard;
 
     // Use this for initialization
     void Start()
@@ -39,30 +40,41 @@ public class Player_Info_Ingame : MonoBehaviour {
         wentThrough = new bool[GameObject.Find("Checkpoints").GetComponent<Checkpoints_Check>().checkpoints_collider.Length];
         distanceToWaypoint = new float[GameObject.Find("Checkpoints").GetComponent<Checkpoints_Check>().wayPoints.Length];
         players = GameObject.FindGameObjectsWithTag("Player");
+        playersLeaderboard = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update () {
         if(GameObject.Find("Checkpoints").GetComponent<Checkpoints_Check>().initializedWaypointDistances)
         {
-            foreach (GameObject player in players)
+            for (int i=0;i<players.Length-1;i++)
             {
-                if (GetComponent<Player_Info_Ingame>().distanceToWaypoint[0] > player.GetComponent<Player_Info_Ingame>().distanceToWaypoint[0])
-                    leaderboardPosition = player.GetComponent<Player_Info_Ingame>().leaderboardPosition + 1;
+                for (int j = 0; j < players.Length-1; j++)
+                {
+                    if (players[i].GetComponent<Player_Info_Ingame>().distanceToWaypoint[0] > players[i+1].GetComponent<Player_Info_Ingame>().distanceToWaypoint[0])
+                    {
+                        GameObject tmp = playersLeaderboard[i + 1];
+                        playersLeaderboard[i + 1] = playersLeaderboard[i];
+                        playersLeaderboard[i] = tmp;
+                    }
+                }
             }
         }
-        /*foreach(GameObject player in players)
+        for (int i = 0; i < playersLeaderboard.Length - 1; i++)
+        {
+            if (playersLeaderboard[i].GetComponent<Player_Info_Ingame>().isLocalPlayer)
+                players[i].GetComponent<Player_Info_Ingame>().leaderboardPosition = i+1;
+        }
+        foreach (GameObject player in players)
         {
             if(!player.GetComponent<Player_Info_Ingame>().isLocalPlayer)
             {
                 if (GetComponent<Player_Info_Ingame>().lap_count < player.GetComponent<Player_Info_Ingame>().lap_count)
                     leaderboardPosition= player.GetComponent<Player_Info_Ingame>().leaderboardPosition + 1;
                 else if(GetComponent<Player_Info_Ingame>().lap_count > player.GetComponent<Player_Info_Ingame>().lap_count)
-                {
-
-                }
+                    leaderboardPosition = player.GetComponent<Player_Info_Ingame>().leaderboardPosition - 1;
             }
-        }*/
+        }
         GameObject.Find("PositionText").GetComponent<Text>().text = leaderboardPosition.ToString();
 
     }
