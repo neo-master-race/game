f557e0ac8d46d23c17280619239eed375dc3d36c
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-28 13:22:42 +0200

    disabled some parts of the car controller by default for non local players

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/car_controller/CarController.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/car_controller/CarController.cs
@@ -71,66 +71,68 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
-        float acceleration = 0.0f;
+        if (isLocalPlayer) {
+            float acceleration = 0.0f;
 
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            float padRotation = Input.acceleration.x * 1.5f;
-
-            if (buttonForward.GetComponent<CustomButton>().down)
+            if (Application.platform == RuntimePlatform.Android)
             {
-                acceleration = 1.0f;
+                float padRotation = Input.acceleration.x * 1.5f;
+
+                if (buttonForward.GetComponent<CustomButton>().down)
+                {
+                    acceleration = 1.0f;
+                }
+                else if (buttonBackward.GetComponent<CustomButton>().down)
+                {
+                    acceleration = -1.0f;
+                }
+                /* if (MaxRotation < Mathf.Abs(padRotation))
+                 {
+                     padRotation = (0.0f < padRotation) ? MaxRotation : -(MaxRotation);
+                 }*/
+                /*
+                 * Zone morte. Si la rotation est inférieure à MinRotation, elle est nulle. 
+                 */
+                float MinRotation = 0.1f;
+                if (Mathf.Abs(padRotation) < MinRotation)
+                {
+                    padRotation = (0.0f < padRotation) ? MinRotation : -MinRotation;
+                }
+                padRotation += (0.0f < padRotation) ? -MinRotation : MinRotation;
+                turnValue = padRotation;
             }
-            else if (buttonBackward.GetComponent<CustomButton>().down)
+            else
             {
-                acceleration = -1.0f;
+                acceleration = Input.GetAxis("Vertical");
+                // Get turning input
+                turnValue = 0.0f;
+                float turnAxis = Input.GetAxis("Horizontal");
+                if (Mathf.Abs(turnAxis) > deadZone)
+                    turnValue = turnAxis;
             }
-            /* if (MaxRotation < Mathf.Abs(padRotation))
-             {
-                 padRotation = (0.0f < padRotation) ? MaxRotation : -(MaxRotation);
-             }*/
-            /*
-             * Zone morte. Si la rotation est inférieure à MinRotation, elle est nulle. 
-             */
-            float MinRotation = 0.1f;
-            if (Mathf.Abs(padRotation) < MinRotation)
+
+            
+
+
+            thrust = 0.0f;
+            if (acceleration > deadZone)
+                thrust = acceleration * forwardAcceleration * currentAcceleration;
+            else if (acceleration < -deadZone)
             {
-                padRotation = (0.0f < padRotation) ? MinRotation : -MinRotation;
+                thrust = acceleration * reverseAcceleration * currentAcceleration;
+                turnValue *= -1f;
             }
-            padRotation += (0.0f < padRotation) ? -MinRotation : MinRotation;
-            turnValue = padRotation;
-        }
-        else
-        {
-            acceleration = Input.GetAxis("Vertical");
-            // Get turning input
-            turnValue = 0.0f;
-            float turnAxis = Input.GetAxis("Horizontal");
-            if (Mathf.Abs(turnAxis) > deadZone)
-                turnValue = turnAxis;
-        }
-
-        
+            vitesse = thrust;// + Mathf.Abs(turnValue * turnStrength);
 
 
-        thrust = 0.0f;
-        if (acceleration > deadZone)
-            thrust = acceleration * forwardAcceleration * currentAcceleration;
-        else if (acceleration < -deadZone)
-        {
-            thrust = acceleration * reverseAcceleration * currentAcceleration;
-            turnValue *= -1f;
-        }
-        vitesse = thrust;// + Mathf.Abs(turnValue * turnStrength);
-
-
-        // limit the message per second rate
-        limiter += 1;
-        limiter = limiter % 6;
-        // if the player moved, send his new position
-        if (isLocalPlayer && limiter == 0 && vitesse != 0)
-        {
-            updatePlayerPosition();
+            // limit the message per second rate
+            limiter += 1;
+            limiter = limiter % 6;
+            // if the player moved, send his new position
+            if (isLocalPlayer && limiter == 0 && vitesse != 0)
+            {
+                updatePlayerPosition();
+            }
         }
     }
 
