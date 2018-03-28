7b0925e90f91c301fc01e74e9de1642718cebfdc
Demanze <keiran1337@gmail.Com>
2018-03-28 13:43:40 +0200

    rechange car sensivity

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/car_controller/CarController.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/car_controller/CarController.cs
@@ -76,7 +76,7 @@ public class CarController : MonoBehaviour
 
             if (Application.platform == RuntimePlatform.Android)
             {
-                float padRotation = Input.acceleration.x * 2.0f;
+                float padRotation = Input.acceleration.x * 2.5f;
 
                 if (buttonForward.GetComponent<CustomButton>().down)
                 {
