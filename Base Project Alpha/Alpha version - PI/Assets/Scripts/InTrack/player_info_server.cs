9192a7adf1f2fddfb08c3a09836f03c9caee5a19
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-03-31 17:05:21 +0200

    race control server test

---
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/InTrack/player_info_server.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_info_server : MonoBehaviour {
+
+    public int[] players_laps;
+
+    public GameObject[] players;
+    public GameObject[] playersLeaderboard;
+
+    // Use this for initialization
+    void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < players.Length - 1; i++)
+        {
+            players_laps[i] = players[i].GetComponent<Player_Info_Ingame>().lap_count;
+        }
+    }
+}
