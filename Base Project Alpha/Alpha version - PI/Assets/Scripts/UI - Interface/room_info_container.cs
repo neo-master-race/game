157bb905b753de182ecdf444e3d6cc1dd2eb406c
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-04-21 00:26:19 +0200

    working room display system, can adapt with number of room

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/room_info_container.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/room_info_container.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public enum RoomType { SingleRace, Tournament };
@@ -21,10 +22,36 @@ public class Room
 
 public class room_info_container : MonoBehaviour {
 
+    [Header("Room Creation GameObjects")]
     public GameObject roomParent;
     public GameObject roomSinglePrefab;
     public GameObject roomTournamentPrefab;
+
+    [Header("Circuit Images")]
+    public Texture track1Texture;
+    public Texture track2Texture;
+    //public Texture track3Texture;
+
+    [Header("Room Creation")]
     public Room[] rooms;
+
+
+    void setRoomAttributes(GameObject room,string roomGameMode,int roomIndex)
+    {
+        if(roomGameMode== "SingleRace")
+        {
+            room.transform.Find("RaceMode").GetComponent<Text>().text = "COURSE SIMPLE";
+            if (rooms[roomIndex].circuits[0] == Circuit.Track1)
+                room.transform.Find("Circuit").GetComponent<RawImage>().texture = track1Texture;
+            else if (rooms[roomIndex].circuits[0] == Circuit.Track2)
+                room.transform.Find("Circuit").GetComponent<RawImage>().texture = track2Texture;
+            //else if (rooms[roomIndex].circuits[0] == Circuit.Track3)
+            //    room.transform.Find("Circuit1").GetComponent<RawImage>().texture = track3Texture;
+        }
+        else
+            room.transform.Find("RaceMode").GetComponent<Text>().text = "TOURNOI";
+        room.transform.Find("PlayerNB").GetComponent<Text>().text = rooms[roomIndex].currentPlayersNb + "/" + rooms[roomIndex].MaximumPlayersNb;
+    }
 
     // Use this for initialization
     void Start () {
@@ -33,13 +60,29 @@ public class room_info_container : MonoBehaviour {
             if (rooms[i].room == RoomType.SingleRace)
             {
                 GameObject singleRaceRoom = Instantiate(roomSinglePrefab, roomParent.transform);
+                setRoomAttributes(singleRaceRoom, "SingleRace",i);
+                singleRaceRoom.transform.GetComponent<RectTransform>().anchoredPosition =
+                    new Vector2
+                    (
+                        singleRaceRoom.transform.GetComponent<RectTransform>().anchoredPosition.x,
+                        -25 - 105 * i
+                    );
             }
             else
             {
                 GameObject tournamentRoom = Instantiate(roomTournamentPrefab, roomParent.transform);
+                setRoomAttributes(tournamentRoom, "Tournament",i);
+                tournamentRoom.transform.GetComponent<RectTransform>().anchoredPosition =
+                    new Vector2
+                    (
+                        tournamentRoom.transform.GetComponent<RectTransform>().anchoredPosition.x,
+                        -25 - 105 * i
+                    );
             }
         }
-	}
+        if(rooms.Length>4)
+            GameObject.Find("Rooms").GetComponent<RectTransform>().offsetMin = new Vector2(GameObject.Find("Rooms").GetComponent<RectTransform>().offsetMin.x, -105*(rooms.Length-4));
+    }
 	
 	// Update is called once per frame
 	void Update () {
