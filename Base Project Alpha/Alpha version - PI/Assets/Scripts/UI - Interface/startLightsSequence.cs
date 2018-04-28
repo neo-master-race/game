8518692581a1f5f4f0c548457334d3f9d3c3004d
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-04-28 21:02:14 +0200

    added 5 lights before start of the race controlling race start

---
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/startLightsSequence.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startLightsSequence : MonoBehaviour {
+
+    public float timerInBetweenLights = 1.0f;
+
+    public Texture blackLight;
+    public Texture redLight;
+    public Texture greenLight;
+
+    private int lightSequenceStep = 0;
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Stratos").GetComponent<CarController>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerInBetweenLights -= Time.deltaTime;
+        if(timerInBetweenLights<0 && lightSequenceStep<=7)
+        {
+            lightSequenceStep++;
+
+            if (lightSequenceStep <= 5)
+                GameObject.Find("Light" + lightSequenceStep).GetComponent<RawImage>().texture = redLight;
+
+            timerInBetweenLights = 1.0f;
+
+            if(lightSequenceStep == 5)
+                timerInBetweenLights = 1.0f+Random.Range(1f,3f);
+
+            if (lightSequenceStep==6)
+            {
+                for(int i=1;i<=5;i++)
+                {
+                    GameObject.Find("Light" + i).GetComponent<RawImage>().texture = greenLight;
+                }
+                GameObject.Find("Stratos").GetComponent<CarController>().enabled = true;
+                timerInBetweenLights = 2.0f;
+            }
+            if (lightSequenceStep == 7)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    GameObject.Find("Light" + i).SetActive(false);
+                }
+            }
+        }
+	}
+}
