b51e040f9ecb8740ba589ee432d4230031fadec7
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-05-12 11:31:58 +0200

    chnaged waiting for opponent script

--- Base Project Alpha/Alpha version - PI/Assets/Scripts/InTrack/waiting_opponents.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/InTrack/waiting_opponents.cs
@@ -13,11 +13,16 @@ public class waiting_opponents : MonoBehaviour {
     void Start () {
         //Debug.Log(GameObject.Find("2nd_Place").transform.position);
         //GameObject.Find("Stratos_AI_Position").transform.position = new Vector3(GameObject.Find("2nd_Place").transform.position.x, 0.67f, GameObject.Find("2nd_Place").transform.position.z+2f);
+        /*set_position();
+        GameObject.Find("Lamborghini").transform.position =
+                    new Vector3(GameObject.Find("Place1").transform.position.x,
+                    0.67f,
+                    GameObject.Find("Place1").transform.position.z);*/
     }
 
     void set_position ()
     {
-        int random = Random.Range(1, (GameObject.Find("Race_Control").GetComponent<player_info_server>().players.Length + 1));
+        int random = Random.Range(1, (GameObject.Find("RaceInformations").GetComponent<RaceInformations>().players.Length + 1));
         if (placement_int.Contains(random))
             set_position();
         else
@@ -27,15 +32,15 @@ public class waiting_opponents : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         
-		if(!hasStartedProcedure && GameObject.Find("Race_Control").GetComponent<player_info_server>().players.Length>=2)
+		if(!hasStartedProcedure && GameObject.Find("RaceInformations").GetComponent<RaceInformations>().players.Length>=2)
         {
-            for(int i=0;i< GameObject.Find("Race_Control").GetComponent<player_info_server>().players.Length; i++)
+            for(int i=0;i< GameObject.Find("RaceInformations").GetComponent<RaceInformations>().players.Length; i++)
             {
                 set_position();
-                GameObject.Find("Race_Control").GetComponent<player_info_server>().players[i].transform.position= 
+                GameObject.Find("RaceInformations").GetComponent<RaceInformations>().players[i].transform.position= 
                     new Vector3(GameObject.Find("Place" + placement_int[i]).transform.position.x,
                     0.67f, 
-                    GameObject.Find("Place"+placement_int[i]).transform.position.z + 2f);
+                    GameObject.Find("Place"+placement_int[i]).transform.position.z);
             }
             hasStartedProcedure = true;
         }
