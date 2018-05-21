68b53aaa550f15d3471ec5d768e91228b76a64be
Luc-Michel Zissler <luc-michel.zissler@etu.unistra.fr>
2018-05-22 00:23:19 +0200

    Ajout d'écran titre

---
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/BlinkText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkText : MonoBehaviour {
+
+    public CanvasGroup UIText;
+    public float animationDuration = 0.75f;
+
+    public void fadeIn()
+    {
+        StartCoroutine(Fadetext(UIText, UIText.alpha, 1, animationDuration,1));
+    }
+
+    public void fadeOut()
+    {
+        StartCoroutine(Fadetext(UIText, UIText.alpha, 0, animationDuration,0));
+    }
+
+    public IEnumerator Fadetext(CanvasGroup UIText, float start, float end, float lerpTime,int state)
+    {
+        float startTime = Time.time;
+        float currentAnimTime = Time.time - startTime;
+        float progress = currentAnimTime / lerpTime;
+
+
+        while (true)
+        {
+            currentAnimTime = Time.time - startTime;
+            progress = currentAnimTime / lerpTime;
+
+            UIText.alpha = Mathf.Lerp(start, end, progress);
+
+            if (progress >= 1)
+                break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        if(state == 0)
+        {
+            fadeIn();
+        }
+        else
+        {
+            fadeOut();
+        }
+    }
+
+    void Start()
+    {
+        fadeOut();    
+    }
+
+}
---
+++ Base Project Alpha/Alpha version - PI/Assets/Scripts/UI - Interface/FadeOutTitleScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutTitleScreen : MonoBehaviour
+{
+
+    public GameObject uiTitleScreengo;
+    public CanvasGroup uiTitleScreen;
+
+
+    public float animationDuration = 0.25f;
+
+    public void fadeOut()
+    {
+        StartCoroutine(FadeTitle(uiTitleScreen, uiTitleScreen.alpha, 0, animationDuration));
+    }
+
+    public IEnumerator FadeTitle(CanvasGroup uiText, float start, float end, float lerpTime)
+    {
+        float startTime = Time.time;
+        float currentAnimTime = Time.time - startTime;
+        float progress = currentAnimTime / lerpTime;
+
+
+        while (true)
+        {
+            currentAnimTime = Time.time - startTime;
+            progress = currentAnimTime / lerpTime;
+
+            uiText.alpha = Mathf.Lerp(start, end, progress);
+
+            if (progress >= 1)
+                break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        uiTitleScreengo.SetActive(false);
+        
+    }
+}
