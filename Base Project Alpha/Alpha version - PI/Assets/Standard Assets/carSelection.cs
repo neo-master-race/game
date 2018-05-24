c3ced03c9b3d4a3a3f9b48ea833b010318689ebe
Schneberger Maxime <maxime.schneberger@gmail.com>
2018-05-24 10:22:20 +0200

    added car podium on car selection

--- Base Project Alpha/Alpha version - PI/Assets/Standard Assets/carSelection.cs
+++ Base Project Alpha/Alpha version - PI/Assets/Standard Assets/carSelection.cs
@@ -8,6 +8,11 @@ public class carSelection : MonoBehaviour {
     public GameObject vehiculeNameText;
     public GameObject selectionGoLeft;
     public GameObject selectionGoRight;
+    public GameObject carPodium;
+    public GameObject stratos;
+    public GameObject porsche;
+    public GameObject lambo;
+    public GameObject ford;
 
     public int selectedVehicule = 0;
     private int totalVehiculeNumber = 4;
@@ -18,15 +23,31 @@ public class carSelection : MonoBehaviour {
         {
             case 0:
                 vehiculeNameText.GetComponent<Text>().text = "Stratos";
+                stratos.SetActive(true);
+                porsche.SetActive(false);
+                lambo.SetActive(false);
+                ford.SetActive(false);
                 break;
             case 1:
                 vehiculeNameText.GetComponent<Text>().text = "Porsche";
+                stratos.SetActive(false);
+                porsche.SetActive(true);
+                lambo.SetActive(false);
+                ford.SetActive(false);
                 break;
             case 2:
                 vehiculeNameText.GetComponent<Text>().text = "Lamborghini";
+                stratos.SetActive(false);
+                porsche.SetActive(false);
+                lambo.SetActive(true);
+                ford.SetActive(false);
                 break;
             case 3:
                 vehiculeNameText.GetComponent<Text>().text = "Ford";
+                stratos.SetActive(false);
+                porsche.SetActive(false);
+                lambo.SetActive(false);
+                ford.SetActive(true);
                 break;
 
             default:
